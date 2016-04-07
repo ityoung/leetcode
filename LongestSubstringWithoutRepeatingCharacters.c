42259307de954dc8d3a82439f533e47fdb7c98f9
ityoung <ityoung@126.com>
2016-04-07 14:39:34 +0800

    Create LongestSubstringWithoutRepeatingCharacters.c

---
+++ LongestSubstringWithoutRepeatingCharacters.c
@@ -0,0 +1,28 @@
+//O(N^2)
+int lengthOfLongestSubstring(char* s) {
+    int len = strlen(s), temp = 0, maxlen = 0, equal = 1;
+    int index1 = 0,index2 = 1,index3;
+    if(len<2)
+    	return len;
+    for(; index1<len-1; index1 = ++index3){
+    	while(index2<len){
+	    	for(index3 = index1; index3<index2; index3++){
+	    		equal = s[index2] - s[index3];
+	    		if(!equal){   //same characters
+	    			temp = index2-index3;
+	    			maxlen = maxlen>temp?maxlen:temp;
+	    			break;
+		    	}
+	    	}
+   			index2++;
+	    	if(!equal){
+	    		break;
+	    	}
+	    	else{   //no same characters bettween index1 and index2
+	    		temp = index2-index1;
+    			maxlen = maxlen>temp?maxlen:temp;
+	    	}
+	    }
+    }
+	return maxlen;
+}
