0e04be91a2246a8215dffd3b3c387a06154f0b4f
ityoung <ityoung@126.com>
2016-04-18 16:14:04 +0800

    Update LongestCommonPrefix.c

--- Algorithms/LongestCommonPrefix.c
+++ Algorithms/LongestCommonPrefix.c
@@ -7,12 +7,12 @@ char* longestCommonPrefix(char** strs, int strsSize) {
     lc = strlen(temp);
     for(i = 1; i<strsSize; i++)
     	for(j = 0;j<lc;j++)
-			if(strs[i][j]==temp[j])
-				continue;
-			else{
-		    	lc = j+1;
-		    	temp[j] ='\0';
-		    	break;
-			}
+	    if(strs[i][j]==temp[j])
+		continue;
+	    else{
+		lc = j+1;
+		temp[j] ='\0';
+		break;
+	    }
     return temp;
 }
